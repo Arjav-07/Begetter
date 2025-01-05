851f9e8bb99c8c6db48f37b5fb5d4042af802803
ArjavBhisara07 <arjavbhisara07@gmail.com>
2025-01-06 02:15:54 +0530

    BEGETTER 1

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget
         child: Center(
           child: Container(
             child: const Text("Welcome to Begetter"),
+            
           ),
         ),
       ),
