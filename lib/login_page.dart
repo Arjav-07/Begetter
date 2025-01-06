15db1b45462079aad3a7a55f14cd43e62dfd8d40
ArjavBhisara07 <arjavbhisara07@gmail.com>
2025-01-06 13:15:45 +0530

    Fifth commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:begetter/utils/routes.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -6,67 +7,62 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.white,
-      child: Column(
-        children: [
-          // Login image
-          Image.asset(
-            "assets/login_image.png",
-            fit: BoxFit.cover,
-          ),
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Login image
+              Image.asset(
+                "assets/login_image.png",
+                fit: BoxFit.cover,
+              ),
 
-          const SizedBox(
-            height: 20.0,
-          ),
+              const SizedBox(
+                height: 20.0,
+              ),
 
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 42),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Enter Username", // Changed to "Username"
-                    labelText: "Username", // Changed to "Username"
-                  ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 16, horizontal: 42),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Enter Username", // Changed to "Username"
+                        labelText: "Username", // Changed to "Username"
+                      ),
+                    ),
+                    // Password text field
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Enter Password",
+                        labelText: "Password",
+                      ),
+                      obscureText: true, // Hide password text
+                    ),
+
+                    const SizedBox(
+                      height: 20.0,
+                    ),
+
+                    //elevated button for login
+                    ElevatedButton(
+                      style: TextButton.styleFrom(),
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.HomeRoute);
+                      },
+                      child: const Text("Login"),
+                    )
+                  ],
                 ),
-                // Password text field
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Enter Password",
-                    labelText: "Password",
-                  ),
-                  obscureText: true, // Hide password text
-                ),
+              ),
+              // Username text field
 
-                
-                const SizedBox(
-                  height: 20.0,
-                ),
-
-                //elevated button for login
-                ElevatedButton(
-                  style: TextButton.styleFrom(),
-                  onPressed: () {
-                    print("Hi Begetter");
-                  },
-                  child: Text("Login"),
-                )
-              ],
-            ),
+              const SizedBox(
+                height: 40.0,
+              ),
+            ],
           ),
-          // Username text field
-
-          const SizedBox(
-            height: 20.0,
-          ),
-
-          // Welcome text
-          const Text(
-            "Welcome to login",
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-        ],
-      ),
-    );
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:begetter/login_page.dart';
 import 'package:begetter/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:begetter/utils/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,21 +14,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        initialRoute: '/login', // Set initial route to be LoginPage
-        themeMode: ThemeMode.dark, // Dark theme as default
-        theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          fontFamily: GoogleFonts.lato().fontFamily, // Use Lato font family
-          textTheme:
-              GoogleFonts.latoTextTheme(), // Apply Lato font to textTheme
-        ),
-        darkTheme: ThemeData(
-          brightness: Brightness.dark, // Apply dark theme
-        ),
-        routes: {
-          "/": (context) => LoginPage(),
-          "/home": (context) => HomePage(),
-          "/login": (context) => LoginPage()
-        });
+      initialRoute: '/login', // Set initial route to be LoginPage
+      themeMode: ThemeMode.dark, // Dark theme as default
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily, // Use Lato font family
+        textTheme: GoogleFonts.latoTextTheme(), // Apply Lato font to textTheme
+      ),
+      darkTheme: ThemeData(brightness: Brightness.light),
+      routes: {
+        '/': (context) => const LoginPage(),
+        MyRoutes.HomeRoute: (context) => HomePage(),
+        MyRoutes.loginRoute: (context) => const LoginPage(),
+      },
+    );
   }
 }
