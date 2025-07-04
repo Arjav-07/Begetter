cdf22f649bc94aca53290f38968a65047730e854
Arjav-07 <arjavbhisara07@gmail.com>
2025-07-04 11:29:25 +0530

    AGP fixed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:begetter/core/store.dart';
 import 'package:begetter/models/cart.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:velocity_x/velocity_x.dart';
 import 'package:begetter/models/catalog.dart';
